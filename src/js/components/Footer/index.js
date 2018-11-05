0bdaa085972e58c8362ab49c845576dc89a80f32
lastresmoya <lastresmoyallc@gmail.com>
2018-11-06 02:13:33 +0800

    jesus, logic for the overallstage

--- src/js/components/Footer/index.js
+++ src/js/components/Footer/index.js
@@ -3,9 +3,6 @@ import React, {Component} from "react";
 class Footer extends Component{
 	constructor(props){
 		super(props);
-		// this.state = {
-		// 	overallStage: "Overall stage cannot be calculated",
-		// };
 	}
 	render(){
 		const { Ch, O, L, E } = this.props;
@@ -20,17 +17,17 @@ class Footer extends Component{
 			);
 		};
 		let renderStage = () => {
-			// logic for Stage Sum
+			// Logic for Stage Number
 			let valueSum = 0;
 			const numValues = [Ch.num, O.num, L.num, E.num];
-
+            // Sum all values of num that are numbers
 			for (let index = 0; index < numValues.length; index++) {
 				const element = numValues[index];
 				if (typeof (element) === "number") {
 					valueSum = element + valueSum;
 				}
 			}
-			// Switch Statement depending on valueSum
+			// Switch Statement depending on valueSum of Numbers selected
 			let stageValue = (valueSum) => {
 				if (valueSum >= 1 && valueSum <= 3) {
 					return "I";
@@ -44,22 +41,25 @@ class Footer extends Component{
 			};
 			return `${stageValue(valueSum)}`;
 		};
-		let renderX = () => {
-			// Logic for X
-			// if(){
-
-			// }
-			return "x";
+		let renderPostFix = () => {
+            // IF any of the values of Num is an "X" , 
+            // return a string of X, else return empty string
+			if (Ch.num === "X" || O.num === "X" || L.num === "X" || E.num === "X"){
+				return "x";
+			} else return "";
 		};
 		let renderOverAllStage = () => {
 			if (Ch.num === "X" && O.num === "X" && L.num === "X" && E.num === "X") {
+                // If all the values of num are an "X", 
+                // return this message
 				return "Overall stage cannot be calculated";
 			}else {
+                // If there's no value of Num that is an "X" 
+                // then render stage and postfix
 				return (
 					<span>
 						{renderStage()}
-						{renderX()}
-						{/* {!this.state.stateX && ""} */}
+						{renderPostFix()}
 					</span>
 				);
 			}
